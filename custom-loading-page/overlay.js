6066ec6b59417875ae1e60f1a527efd0bc0b7143
valerie-m <68434174+valerie-m@users.noreply.github.com>
2020-07-19 16:34:44 -0400

    Create overlay.js

---
+++ custom-loading-page/overlay.js
@@ -0,0 +1,7 @@
+var overlay = document.getElementById("overlay"); //contains reference to div with id overlay in HTML file
+
+window.addEventListener('load', function() {
+	overlay.style.display = 'none';
+});
+
+
